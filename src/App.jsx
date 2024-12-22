8298054034fcb4dec330c01f4a627c1a60363bfe
Prashant <hadaprashant2002@gmail.com>
2024-12-22 16:20:40 +0530

    Created basic component file structure and Header componet along with router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    <Outlet />
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './components/Home/Home.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element= {<App />}>
+    <Route path="" element={<Home />} />
+  </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router= {router}/>
   </StrictMode>,
 )
